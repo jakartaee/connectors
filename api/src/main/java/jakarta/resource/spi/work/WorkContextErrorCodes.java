94c7709c031978dc5befa8dcfb0efac72b947121
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-12-14 17:39:56 +0100

    Revert "Add default constructors"
    This reverts commit de7faa25913ccab8cf64b2dbd5a5693c8a228a19.

--- api/src/main/java/jakarta/resource/spi/work/WorkContextErrorCodes.java
+++ api/src/main/java/jakarta/resource/spi/work/WorkContextErrorCodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -28,12 +28,6 @@ package jakarta.resource.spi.work;
  * @since 1.6
  */
 public class WorkContextErrorCodes {
-
-	/**
-	 * Default constructor.
-	 */
-	protected WorkContextErrorCodes() {
-	}
 
 	/**
 	 * Indicates that a <code>WorkContext</code> type, that was not specified as
